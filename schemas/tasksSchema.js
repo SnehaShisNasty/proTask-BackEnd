45ba1f6618b82769508ac7cb70f6ee7da9654550
Dima <dimasalabay0711@gmail.com>
2024-05-11 13:51:55 +0300

    fixing

--- schemas/tasksSchema.js
+++ schemas/tasksSchema.js
@@ -3,13 +3,13 @@ import Joi from "joi";
 export const createTaskSchema = Joi.object({
 	title: Joi.string().required(),
 	description: Joi.string().required(),
-	color: Joi.string().required(),
+	priority: Joi.string().required(),
 	deadline: Joi.string().required(),
 });
 
 export const editTaskSchema = Joi.object({
 	title: Joi.string(),
 	description: Joi.string(),
-	color: Joi.string(),
+	priority: Joi.string(),
 	deadline: Joi.string(),
 });
